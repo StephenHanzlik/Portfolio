02c5cd3ce5538ef566a412b82e12e2462503200c
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-02-10 19:47:46 -0700

    styling looks good.  invisible header, cards, summary

--- js/init.js
+++ js/init.js
@@ -1,7 +1,27 @@
-(function($){
-  $(function(){
+(function($) {
+  $(function() {
 
     $('.button-collapse').sideNav();
 
-  }); // end of document ready
+  });
+  jQuery(document).ready(function($) {
+    // Defining a function to set size for #hero
+    function fullscreen() {
+      jQuery('#hero').css({
+        width: jQuery(window).width(),
+        height: jQuery(window).height()
+      });
+    }
+
+    fullscreen();
+
+    // Run the function in case of window resize
+    jQuery(window).resize(function() {
+      fullscreen();
+    });
+
+  });
+
+
+  // end of document ready
 })(jQuery); // end of jQuery name space
